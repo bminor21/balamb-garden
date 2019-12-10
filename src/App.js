84bd307a21ee51dcbcb7a5ad7f4a70f007c79fd6
Brett Minor <brett.minor21@gmail.com>
2019-12-10 16:22:57 -0500

    updated to use hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import Person from './Person/Person';
+
+const App = (props) => {
+  const [person, setPerson] = useState({name: 'Brett', age: 31});
+
+  const handleNameSwitch = () => {
+    setPerson({name: 'Daniel', age: 29});
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <button onClick={handleNameSwitch}>Switch Name</button>
+      <Person name={person.name} age={person.age}>My hobbies are: Gaming, Lifting</Person>
+    </div>
+  );
 }
 
 export default App;
