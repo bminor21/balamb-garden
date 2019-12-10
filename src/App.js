4cce91cff32770c153af7496ed22288464356aa0
Brett Minor <brett.minor21@gmail.com>
2019-12-10 16:24:25 -0500

    reverted to class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Person from './Person/Person';
 
-const App = (props) => {
-  const [person, setPerson] = useState({name: 'Brett', age: 31});
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Brett', age: 31},
+      { name: 'Rikku', age: 26 }
+    ]
+  }
 
-  const handleNameSwitch = () => {
-    setPerson({name: 'Daniel', age: 29});
+  handleNameSwitch = () => {
+    this.setState({
+      persons: [
+        { name: 'Daniel', age: 34 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
   };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <button onClick={handleNameSwitch}>Switch Name</button>
-      <Person name={person.name} age={person.age}>My hobbies are: Gaming, Lifting</Person>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <button onClick={this.handleNameSwitch}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies are: Gaming, Lifting</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+      </div>
+    );
+  }
 }
 
 export default App;
